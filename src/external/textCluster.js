0ae5e24efdcf950deee5417f8da15c58e05e98f8
jinraj <38273588+jinraj@users.noreply.github.com>
2020-06-01 15:10:22 +0530

    domain name is added

--- src/external/textCluster.js
+++ src/external/textCluster.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { getUtterance, setUtterances } from '../global/appVariable'
 
+const domain = "http://ec2-18-204-11-37.compute-1.amazonaws.com";
+
 function getIntents(slotValues, inputValues) {
-    const path = "/intents";
+    const path = domain + "/intents";
     const data = { inputValues: inputValues, slotValues: slotValues, utterances: getUtterance() };
 
     return new Promise((resolve, reject) => {
@@ -17,7 +19,7 @@ function getIntents(slotValues, inputValues) {
 }
 
 function getSlots(value) {
-    const path = "/slots";
+    const path = domain + "slots";
     const data = { inputValues: value, utterances: getUtterance() };
     return new Promise((resolve, reject) => {
         axios.post(path, data)
