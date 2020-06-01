d73dfce865f98ac1c062765d94dadffdc8bae325
jinraj <38273588+jinraj@users.noreply.github.com>
2020-06-01 23:13:02 +0530

    Update createBotFiles.js

--- src/botModel/lex/createBotFiles.js
+++ src/botModel/lex/createBotFiles.js
@@ -1,54 +1,54 @@
 
-// import zip from 'jszip';
-// import { saveAs } from 'file-saver';
-// var fs = require("fs")
-// var slotOutDir = './slots-out/'
+import zip from 'jszip';
+import { saveAs } from 'file-saver';
+var fs = require("fs")
+var slotOutDir = './slots-out/'
 
 
 
 function createBotFiles(slots, intents) {
     console.log("slots :", slots);
     console.log("intents :", intents);
-    // createSlot(slots);
+    createSlot(slots);
 }
 
 function createSlot(slots) {
-    // var allSlotDict = {}
+    var allSlotDict = {}
 
-    // for (let [slotName, enumvals] of Object.entries(slots)) {
-    //     console.log(slotName);
-    //     allSlotDict[slotName] = null;
-    //     if (allSlotDict[slotName] == null || allSlotDict[slotName] == undefined) {
-    //         allSlotDict[slotName] = {
-    //             "enumerationValues": enumvals,
-    //             "name": slotName,
-    //             "valueSelectionStrategy": "TOP_RESOLUTION"
-    //         }
-    //     }
-    // };
+    for (let [slotName, enumvals] of Object.entries(slots)) {
+        console.log(slotName);
+        allSlotDict[slotName] = null;
+        if (allSlotDict[slotName] == null || allSlotDict[slotName] == undefined) {
+            allSlotDict[slotName] = {
+                "enumerationValues": enumvals,
+                "name": slotName,
+                "valueSelectionStrategy": "TOP_RESOLUTION"
+            }
+        }
+    };
 
-    // // if (!fs.existsSync(slotOutDir)) {
-    // //   fs.mkdirSync(slotOutDir);
-    // // }
+    // if (!fs.existsSync(slotOutDir)) {
+    //   fs.mkdirSync(slotOutDir);
+    // }
 
-    // var slotvals = {}
-    // Object.keys(allSlotDict).forEach((key) => {
-    //     console.log(key, allSlotDict[key]);
-    //     zip.folder("slots").file(key, allSlotDict[key])
-    //     // let tmp = allSlotDict[key].enumerationValues
-    //     // tmp.forEach(val => {
-    //     //     slotvals[val.value] = val.synonyms
-    //     // })
-    //     //   fs.writeFile(`${slotOutDir}${key}`, JSON.stringify(allSlotDict[key], null, 4), 'utf8', function (err, result) {
-    //     //     if (err) console.log('error', err);
-    //     //   });
-    // });
+    var slotvals = {}
+    Object.keys(allSlotDict).forEach((key) => {
+        console.log(key, allSlotDict[key]);
+        zip.folder("slots").file(key, allSlotDict[key])
+        // let tmp = allSlotDict[key].enumerationValues
+        // tmp.forEach(val => {
+        //     slotvals[val.value] = val.synonyms
+        // })
+        //   fs.writeFile(`${slotOutDir}${key}`, JSON.stringify(allSlotDict[key], null, 4), 'utf8', function (err, result) {
+        //     if (err) console.log('error', err);
+        //   });
+    });
 
-    // zip.generateAsync({ type: "blob" })
-    //     .then(function (content) {
-    //         saveAs(content, "example.zip");
-    //     });
-    //console.log(JSON.stringify(slotvals));
+    zip.generateAsync({ type: "blob" })
+        .then(function (content) {
+            saveAs(content, "example.zip");
+        });
+    console.log(JSON.stringify(slotvals));
 }
 
 export default createBotFiles;
